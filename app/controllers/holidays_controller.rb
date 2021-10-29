e6b9ecd5cd29f0d3cff7f907a98aacf2b180360c
Philip <philipmjobe@gmail.com>
2021-10-29 14:56:11 -0400

    finally got it to work

---
+++ app/controllers/holidays_controller.rb
@@ -0,0 +1,6 @@
+class HolidaysController < ApplicationController
+  def index
+    response = HTTP.get("https://holidays.abstractapi.com/v1/?api_key=&country=US&year=2021&month=01&day=01")
+    render json: response.parse(:json)
+  end 
+end
